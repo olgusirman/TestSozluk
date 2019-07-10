0ee34d4d3e3ef196b21101b45bd73ff99e53ae33
Olgu Sırman <olgu.sirman@vngrs.com>
2019-07-10 11:50:43 +0300

    Initial Commit

---
+++ TestSozluk/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TestSozluk
+//
+//  Created by Olgu Sırman on 10.07.2019.
+//  Copyright © 2019 MesutGunes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
