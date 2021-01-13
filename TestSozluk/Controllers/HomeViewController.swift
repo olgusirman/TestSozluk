55c6f7eaae610175b731f4ba1fe359f9cf2ab8c2
gunesmes <gunesmes@gmail.com>
2021-01-13 23:55:08 +0300

    readme updated

--- TestSozluk/Controllers/HomeViewController.swift
+++ TestSozluk/Controllers/HomeViewController.swift
@@ -10,30 +10,60 @@ import Foundation
 import UIKit
 import MarkdownView
 import WebKit
+import SafariServices
+
 
 final class HomeViewController: UIViewController {
     
+    private let networkManager = NetworkManager()
+    // MARK: Properties
+
     @IBOutlet weak var dictionaryImg: UIImageView!
     @IBOutlet var readmeMd: MarkdownView!
 
-    // MARK: Properties
-    private let networkManager = NetworkManager()
-    
     private var home: Home? {
         didSet {
             readmeMd.load(markdown: home?.readme, enableImage: false)
+
+            self.readmeMd.onTouchLink = { [weak self] request in
+              guard let url = request.url else { return false }
+
+              if url.scheme == "file" {
+                return false
+              } else if url.scheme == "https" {
+                let safari = SFSafariViewController(url: url)
+                self?.navigationController?.pushViewController(safari, animated: true)
+                return false
+              } else {
+                return false
+              }
+            }
+
         }
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
+    override func viewDidLoad() {
         title = "Anasayfa"
+        
         networkManager.getReadme { (home, errorMessage) in
             DispatchQueue.main.async { [self] in
                 self.home = home!
             }
         }
 
+        self.readmeMd.onTouchLink = { [weak self] request in
+          guard let url = request.url else { return false }
+
+          if url.scheme == "file" {
+            return true
+          } else if url.scheme == "https" {
+            let safari = SFSafariViewController(url: url)
+            self?.present(safari, animated: true, completion: nil)
+            return false
+          } else {
+            return false
+          }
+        }
     }
 }
